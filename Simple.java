d3b14b95b82ac0a4860c4bb0637c0fe69ba1951d
crupib <crupib@gmail.com>
2022-02-26 13:46:49 -0500

    initial commit of simple

--- Simple.java
+++ Simple.java
@@ -1,11 +1,5 @@
 class Simple{  
-    public static boolean inTable() {
-     int ntable = 100;
-     int j = 0;
-     return (j == ntable);
-    }
     public static void main(String args[]){  
      System.out.println("Hello Java");  
-     System.out.println(inTable());
     }  
-} 
+}  
